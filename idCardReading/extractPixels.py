9a3f3a1564c09d86a61c6298f2fe1f39be81870b
FreakishNature <eduard.medinschii@gmail.com>
2018-10-28 15:10:23 +0300

    added brightness configuring

--- idCardReading/extractPixels.py
+++ idCardReading/extractPixels.py
@@ -1,36 +1,36 @@
 import cv2
 import numpy as np
 
-def setBrihtness(image,toBrihtness):
+def setBrightness(image,toBrightness):
    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-   brihtness = 0
+   brightness = 0
    for column in gray:
       for row in column:
-            brihtness += row
-   brihtness = brihtness / gray.size / 255
+            brightness += row
+   brightness = brightness / gray.size / 255
 
 
    hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
-   # print(brihtness) 
+   # print(brightness) 
 
-   changeToBrihtness = (brihtness - toBrihtness) * 255       
+   changeToBrightness = (brightness - toBrightness) * 255       
 
-   if(changeToBrihtness < 0):
-      changeToBrihtness *= -1
-      hsv[:,:,2] += int(changeToBrihtness)
+   if(changeToBrightness < 0):
+      changeToBrightness *= -1
+      hsv[:,:,2] += int(changeToBrightness)
    else:
-      hsv[:,:,2] -= int(changeToBrihtness)
+      hsv[:,:,2] -= int(changeToBrightness)
             
    image = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
 
    return image
 
-def extract_pixels(image,brihtness = 0.7):
+def extract_pixels(image,brightness = 0.7):
 
       # image = cv2.resize(image, (1024,768))
-    image = setBrihtness(image,brihtness)
+    image = setBrightness(image,brightness)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
 
