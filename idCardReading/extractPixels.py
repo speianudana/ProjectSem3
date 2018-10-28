2e9f39e437db58fc78f64484e8a49781515f9d2f
FreakishNature <eduard.medinschii@gmail.com>
2018-10-28 15:09:12 +0300

    added brihtness configuring

--- idCardReading/extractPixels.py
+++ idCardReading/extractPixels.py
@@ -1,21 +1,43 @@
 import cv2
 import numpy as np
 
+def setBrihtness(image,toBrihtness):
+   gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-def extract_pixels(image):
-    # TO DO : Test different situation and select best values for any situation 
-    lower_red = np.array([0,0,0])
-    upper_red = np.array([115,255,150])
+   brihtness = 0
+   for column in gray:
+      for row in column:
+            brihtness += row
+   brihtness = brihtness / gray.size / 255
 
+
+   hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+
+   # print(brihtness) 
+
+   changeToBrihtness = (brihtness - toBrihtness) * 255       
+
+   if(changeToBrihtness < 0):
+      changeToBrihtness *= -1
+      hsv[:,:,2] += int(changeToBrihtness)
+   else:
+      hsv[:,:,2] -= int(changeToBrihtness)
+            
+   image = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+
+   return image
+
+def extract_pixels(image,brihtness = 0.7):
+
+      # image = cv2.resize(image, (1024,768))
+    image = setBrihtness(image,brihtness)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
 
-    mask = cv2.inRange(hsv, lower_red , upper_red)
-    # res = cv2.bitwise_and(frame,frame, mask = mask)
-
-
-    # cv2.imshow('start',image)
-    # cv2.imshow('mask',mask)
-    # cv2.imwrite('img-in.png',mask)
-    # cv2.waitKey(0)
+    lower = np.array([0,0,0])  #-- Lower range --
+    upper = np.array([115,255,165])  #-- Upper range --
+    
+    mask = cv2.inRange(hsv, lower, upper)
 
     return mask
+
